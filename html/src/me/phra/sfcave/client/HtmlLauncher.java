1bac61334aa8a75ec5619a776db842f81d7dde2c
kg <kennytgao@gmail.com>
2015-05-09 01:13:58 -0700

    initial commit

---
+++ html/src/me/phra/sfcave/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package me.phra.sfcave.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import me.phra.sfcave.SFCave;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new SFCave();
+        }
+}
