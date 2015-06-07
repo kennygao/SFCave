817657322436773d202801914581ad05bd7925eb
kg <kennytgao@gmail.com>
2015-06-07 02:30:08 -0700

    tweaking configuration

--- core/src/me/phra/sfcave/SFCave.java
+++ core/src/me/phra/sfcave/SFCave.java
@@ -2,26 +2,74 @@ package me.phra.sfcave;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SFCave extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    private SpriteBatch batch;
+    private Texture texture;
+    private Sprite sprite;
+    private BitmapFont debug;
+    private int width;
+    private int height;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        texture = new Texture("badlogic.jpg");
+        sprite = new Sprite(texture);
+        debug = new BitmapFont();
+        debug.setColor(Color.BLACK);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        Gdx.gl.glViewport(0, 0, width, height);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        sprite.setPosition(0, 0);
+        sprite.draw(batch);
+        debug.draw(batch, getDebug(), 0, this.height);
+        batch.end();
+    }
+
+    private String getDebug() {
+        return "SFCave" + "\n" +
+                "height=" + this.height + "\n" +
+                "width=" + this.width + "\n" +
+                "fullscreen=" + Gdx.graphics.isFullscreen() + "\n" +
+                "rendercalls=" + batch.renderCalls + "\n" +
+                "totalrendercalls=" + batch.totalRenderCalls + "\n" +
+                "deltatime=" + Gdx.graphics.getDeltaTime() + "\n" +
+                "fps=" + Gdx.graphics.getFramesPerSecond() + "\n";
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+        debug.dispose();
+        texture.dispose();
+        batch.dispose();
+    }
 }
--- desktop/src/me/phra/sfcave/desktop/DesktopLauncher.java
+++ desktop/src/me/phra/sfcave/desktop/DesktopLauncher.java
@@ -6,7 +6,17 @@ import me.phra.sfcave.SFCave;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SFCave(), config);
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.backgroundFPS = 30;
+		cfg.foregroundFPS = 120;
+		cfg.fullscreen = false;
+		cfg.width = 800;
+		cfg.height = 600;
+		cfg.resizable = false;
+		cfg.title = "SFCave";
+		cfg.vSyncEnabled = false;
+		cfg.x = -1;
+		cfg.y = -1;
+		new LwjglApplication(new SFCave(), cfg);
 	}
 }
